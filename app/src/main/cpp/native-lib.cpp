0275a6576c3042d7726f089fa712afd017231ca1
gabriel marq <gabriel@go2web.com.br>
2017-07-08 03:42:43 -0300

    gradle and mvp structure

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,52 @@
+#include <jni.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <stdio.h>
+
+
+static JavaVM *jvm = NULL;
+static jobject callback = NULL;
+
+void handler(int s) {
+    if(jvm == NULL)
+        return ;
+    if(callback == NULL)
+        return ;
+
+    JNIEnv *env = NULL;
+    jint res;
+    res = (*jvm).AttachCurrentThread((JNIEnv **) (void **)&env, NULL);
+    if(res < 0)
+    {
+        fprintf(stderr, "Attach VM Thread failed\n");
+        return ;
+    }
+
+    jclass cls = (*env).GetObjectClass(callback);
+    jmethodID mid = (*env).GetMethodID(cls, "run", "()V");
+    (*env).CallVoidMethod(callback, mid);
+    (*jvm).DetachCurrentThread();
+}
+
+JNIEXPORT void JNICALL
+Java_br_com_gabriel_muxixi_NativeConvert_asyncConverteToReal(JNIEnv *env, jobject instance,
+                                                             jfloat valueToConvert) {
+    struct sigaction sa;
+    float a = valueToConvert;
+
+    sa.sa_flags = SA_RESTART;
+    sigemptyset (&sa.sa_mask);
+
+    /* find the current jvm */
+    (*env).GetJavaVM(&jvm);
+    /* upgrade callback to global ref */
+    callback = (*env).NewGlobalRef(instance);
+
+    sa.sa_handler = handler;
+
+
+    if (sigaction (SIGALRM/*PROF*/, &sa, NULL) == -1
+        || setitimer (ITIMER_REAL/*PROF*/, &it, NULL) == -1)
+    {
+    }
+}
---
+++ app/src/main/java/br/com/gabriel/muxixi/NativeConvert.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.muxixi;
+
+/**
+ * Created by Gabriel on 04/07/2017.
+ */
+
+public class NativeConvert {
+
+    /*public void native asyncConverteToReal(float valueToConvert);
+    public void nativeCallBack(float convertedValue){
+
+    }*/
+}
